8d6bf48cbc1990710c94ed156caddc92cada0ed7
Rodrigo Borges <42662698+DigoBorges@users.noreply.github.com>
2025-07-22 07:50:38 -0300

    Update Campos obrigatorios.cy.js

--- cypress/e2e/Campos obrigatorios.cy.js
+++ cypress/e2e/Campos obrigatorios.cy.js
@@ -13,22 +13,4 @@ describe('Validação de campos obrigatórios no Login', () => {
 
 
 
-  it.only('exibir critica de campos obrigatorios ao realizar cadastro', () => {
-   
-    cy.get('.ihdmxA').click(); // clicar no botão Registrar
-        cy.get(':nth-child(2) > .input__default'); 
-        cy.get(':nth-child(3) > .input__default'); 
-        cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default');
-        cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default');
-        cy.get('#toggleAddBalance').click({force: true}); 
-        cy.contains('É campo obrigatório').should('be.visible');
- 
-        cy.get('.styles__ContainerFormRegister-sc-7fhc7g-0 > .style__ContainerButton-sc-1wsixal-0').click({force: true}); // clicar no botão cadastrar
-        
-    });
-
-
-
-
-
 });
