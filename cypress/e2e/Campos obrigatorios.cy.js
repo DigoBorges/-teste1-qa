deea737236015767551f0860ef4a4f8dc7e9eda8
DigoBorges <digoblucas@gmail.com>
2025-07-24 09:55:57 -0300

    refatorar testes de cadastro e validação de campos obrigatórios para melhorar a legibilidade e a estrutura

--- cypress/e2e/Campos obrigatorios.cy.js
+++ cypress/e2e/Campos obrigatorios.cy.js
@@ -11,4 +11,52 @@ describe('Validação de campos obrigatórios no Login', () => {
     cy.contains('É campo obrigatório').should('be.visible');  // Valida mensagem de campo obrigatório
   });
 
+
+  it('exibir critica com usuário inválido', () => {
+   cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > :nth-child(1) > .input__default').type("teste@ttt.com",{force: true}); // inserir email de login
+    cy.get('.style__ContainerFormLogin-sc-1wbjw6k-0 > .login__password > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type("abcd1234",{force: true}); // inserir senha de login
+    cy.get('form').should('be.visible'); // validar que formulário aparecendo 
+    cy.get('.otUnI').click(); // clicar no botão Acessar
+         
+     cy.get('#modalText').should('be.visible').and('contain', 'Usuário ou senha inválido').and('contain', 'Tente novamente ou verifique suas informações');  // Valida mensagem de campo obrigatório
+     cy.get('#btnCloseModal').click();
+  });
+
+// Testes de Critica de Senha não Igual para Cadastramento
+
+  it('Critica que as Senhas são diferentes', () => {
+    cy.get('.ihdmxA')
+    .click(); // Abrir tela de cadastro
+
+  
+    cy.get('[name=email]')
+    .eq(1)
+    .type("teste@tt.com",{force: true}); // Preencher campo Email
+    
+    
+    cy.get('[name=name]') // Preencher campo Nome
+      .eq(0)
+      .type("Rodrigo",{force: true}); 
+    
+    cy.get('[name=password]') // Preencher campo senha
+    .eq(1)
+    .type("1234",{force: true}); 
+    
+    cy.get('[name=passwordConfirmation]')  // Preencher campo confirmar Senha
+    .eq(0)
+    .type("teste@tt.com",{force: true});
+    
+    cy.get('#toggleAddBalance')
+    .click({force: true}); // clique no botão de inserir Saldo
+    
+    cy.get('.styles__ContainerFormRegister-sc-7fhc7g-0 > .style__ContainerButton-sc-1wsixal-0')
+    .click({force: true}); // clicar no botão cadastrar
+    
+    cy.get('#btnCloseModal')// fechar a modal
+    .click(); 
+  }); 
+
+
+  
+
 });
